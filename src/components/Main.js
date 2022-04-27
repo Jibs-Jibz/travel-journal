56fdb9d558b9b63fb97d9630e9e5cfcf2bd446c0
jibs-jibz <osunkoyajibola@gmail.com>
2022-04-27 11:23:44 +0100

    upload 1.0

---
+++ src/components/Main.js
@@ -0,0 +1,33 @@
+
+export default function Main(props) {
+    return (<section>
+        <div className="card">
+            <div className="card-img">
+                <img src={props.item.imageUrl} alt="" className="img" />
+            </div>
+            <div className="card-text">
+                <div className="card-text1">
+                    <p className="card-text1-1">
+                        <img src={props.item.imageLocation} />&nbsp;
+                            {props.item.location}
+                            &nbsp;&nbsp;&nbsp;
+                        <span>
+                        {props.item.mapText}
+                        </span>
+                    </p>
+                </div>
+                <div>
+                    <h2>{props.item.title}</h2>
+                </div>
+                <div className="date">
+                    <h4>{props.item.startDate} - {props.item.endDate}</h4>
+                </div>
+                <div className="descr">
+                    <p>{props.item.description}</p>
+                </div>
+            </div>
+        </div>
+        <hr />
+    </section>
+    )
+}
---
+++ src/components/NavBar.js
@@ -0,0 +1,10 @@
+import Earth from "../img/earth-logo.svg"
+
+export default function () {
+    return (
+        <div className="nav">
+            <img src={Earth} />
+            <h3 className="head-text">my travel journal.</h3>
+        </div>
+    )
+}
